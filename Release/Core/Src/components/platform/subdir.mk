e574ab873cfb9fc60b6dfc44b7df272c375195f6
thinkstation <anthony@claydonkey.com>
2023-09-28 11:57:08 +0100

    make files

---
+++ Release/Core/Src/components/platform/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (11.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/components/platform/platform_os.c \
+../Core/Src/components/platform/sdio.c \
+../Core/Src/components/platform/sdio_driver_port.c 
+
+OBJS += \
+./Core/Src/components/platform/platform_os.o \
+./Core/Src/components/platform/sdio.o \
+./Core/Src/components/platform/sdio_driver_port.o 
+
+C_DEPS += \
+./Core/Src/components/platform/platform_os.d \
+./Core/Src/components/platform/sdio.d \
+./Core/Src/components/platform/sdio_driver_port.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/components/platform/%.o Core/Src/components/platform/%.su Core/Src/components/platform/%.cyclo: ../Core/Src/components/platform/%.c Core/Src/components/platform/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -DUSE_HAL_DRIVER -DSTM32F103xE -DUSE_FULL_LL_DRIVER -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM3 -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -I../Core/Src/components/platform/include -I../Core/Src/components/sdio_host/include -Os -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Core-2f-Src-2f-components-2f-platform
+
+clean-Core-2f-Src-2f-components-2f-platform:
+	-$(RM) ./Core/Src/components/platform/platform_os.cyclo ./Core/Src/components/platform/platform_os.d ./Core/Src/components/platform/platform_os.o ./Core/Src/components/platform/platform_os.su ./Core/Src/components/platform/sdio.cyclo ./Core/Src/components/platform/sdio.d ./Core/Src/components/platform/sdio.o ./Core/Src/components/platform/sdio.su ./Core/Src/components/platform/sdio_driver_port.cyclo ./Core/Src/components/platform/sdio_driver_port.d ./Core/Src/components/platform/sdio_driver_port.o ./Core/Src/components/platform/sdio_driver_port.su
+
+.PHONY: clean-Core-2f-Src-2f-components-2f-platform
+
---
+++ Release/Core/Src/subdir.mk
@@ -0,0 +1,63 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (11.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/dma.c \
+../Core/Src/freertos.c \
+../Core/Src/gpio.c \
+../Core/Src/main.c \
+../Core/Src/rtc.c \
+../Core/Src/sdio.c \
+../Core/Src/stm32f1xx_hal_msp.c \
+../Core/Src/stm32f1xx_hal_timebase_tim.c \
+../Core/Src/stm32f1xx_it.c \
+../Core/Src/syscalls.c \
+../Core/Src/sysmem.c \
+../Core/Src/system_stm32f1xx.c \
+../Core/Src/usart.c 
+
+OBJS += \
+./Core/Src/dma.o \
+./Core/Src/freertos.o \
+./Core/Src/gpio.o \
+./Core/Src/main.o \
+./Core/Src/rtc.o \
+./Core/Src/sdio.o \
+./Core/Src/stm32f1xx_hal_msp.o \
+./Core/Src/stm32f1xx_hal_timebase_tim.o \
+./Core/Src/stm32f1xx_it.o \
+./Core/Src/syscalls.o \
+./Core/Src/sysmem.o \
+./Core/Src/system_stm32f1xx.o \
+./Core/Src/usart.o 
+
+C_DEPS += \
+./Core/Src/dma.d \
+./Core/Src/freertos.d \
+./Core/Src/gpio.d \
+./Core/Src/main.d \
+./Core/Src/rtc.d \
+./Core/Src/sdio.d \
+./Core/Src/stm32f1xx_hal_msp.d \
+./Core/Src/stm32f1xx_hal_timebase_tim.d \
+./Core/Src/stm32f1xx_it.d \
+./Core/Src/syscalls.d \
+./Core/Src/sysmem.d \
+./Core/Src/system_stm32f1xx.d \
+./Core/Src/usart.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/%.o Core/Src/%.su Core/Src/%.cyclo: ../Core/Src/%.c Core/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -DUSE_HAL_DRIVER -DSTM32F103xE -DUSE_FULL_LL_DRIVER -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM3 -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -I../Core/Src/components/platform/include -I../Core/Src/components/sdio_host/include -Os -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Core-2f-Src
+
+clean-Core-2f-Src:
+	-$(RM) ./Core/Src/dma.cyclo ./Core/Src/dma.d ./Core/Src/dma.o ./Core/Src/dma.su ./Core/Src/freertos.cyclo ./Core/Src/freertos.d ./Core/Src/freertos.o ./Core/Src/freertos.su ./Core/Src/gpio.cyclo ./Core/Src/gpio.d ./Core/Src/gpio.o ./Core/Src/gpio.su ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/rtc.cyclo ./Core/Src/rtc.d ./Core/Src/rtc.o ./Core/Src/rtc.su ./Core/Src/sdio.cyclo ./Core/Src/sdio.d ./Core/Src/sdio.o ./Core/Src/sdio.su ./Core/Src/stm32f1xx_hal_msp.cyclo ./Core/Src/stm32f1xx_hal_msp.d ./Core/Src/stm32f1xx_hal_msp.o ./Core/Src/stm32f1xx_hal_msp.su ./Core/Src/stm32f1xx_hal_timebase_tim.cyclo ./Core/Src/stm32f1xx_hal_timebase_tim.d ./Core/Src/stm32f1xx_hal_timebase_tim.o ./Core/Src/stm32f1xx_hal_timebase_tim.su ./Core/Src/stm32f1xx_it.cyclo ./Core/Src/stm32f1xx_it.d ./Core/Src/stm32f1xx_it.o ./Core/Src/stm32f1xx_it.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32f1xx.cyclo ./Core/Src/system_stm32f1xx.d ./Core/Src/system_stm32f1xx.o ./Core/Src/system_stm32f1xx.su ./Core/Src/usart.cyclo ./Core/Src/usart.d ./Core/Src/usart.o ./Core/Src/usart.su
+
+.PHONY: clean-Core-2f-Src
+
